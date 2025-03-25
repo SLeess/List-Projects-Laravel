4e944a9cc70ce4e5c1b342a8b64675a9bf8ef592
Lean <duraesleandro21@gmail.com>
2025-03-25 16:22:15 -0300

    Update Client.php

--- larafel_studies_05/app/Models/Client.php
+++ larafel_studies_05/app/Models/Client.php
@@ -20,9 +20,11 @@ class Client extends Model
 
     public function products()
     {
-        return $this->belongsToMany(Product::class, 'orders', 'client_id', 'product_id', 'id', 'id')
-        // ->withPivot('id', 'quantity', 'order_number')
-        // ->withTimestamps()
-        ;
+        return $this->belongsToMany(Product::class, 'orders', 'client_id', 'product_id', 'id', 'id');
+    }
+
+    public function orders()
+    {
+        return $this->hasMany(Order::class, 'client_id', 'id');
     }
 }
